8ab761dc86caefc8bebadebfb1e4cafb44b384f4
Sepehr Ghavam <Sepehr.Ghavam@christiedigital.com>
2022-04-03 15:38:30 -0400

    Switched from config file to docker env vars. Cleaned up dependencies.

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,13 @@ FROM python:3.8-slim-buster
 
 WORKDIR /app
 
+ENV PROXY_HOST="0.0.0.0"
+ENV PROXY_PORT=9899
+ENV CLIENT_HOST="redis-db"
+ENV CLIENT_PORT=6379
+ENV CACHE_CAPACITY=10
+ENV CACHE_EXPIRY=10
+
 COPY requirements.txt requirements.txt
 
 RUN pip3 install -r requirements.txt
--- app.py
+++ app.py
@@ -8,12 +8,12 @@ import sys
 import yaml
 import argparse
 from aiohttp import web
+from os import environ
 
 # todo: add logging
 
 # todo: make sure redis is running
 # todo: make sure redis is configured correctly
-# todo: make sure redis is not running on the same host/port as the proxy
 # todo: add circuit breaker to client calls
 
 # todo: set config params as env vars
@@ -31,40 +31,30 @@ def load_config(config_file):
 async def handle(request):
     key = request.match_info['key']
     try:
-        print(f"Looking for: {key}")
         result = await proxy.get(key)
-    except ConnectionError:
-        return web.Response(text="Proxy server is down", status=500)
     except redis.exceptions.ConnectionError as e:
-        return web.Response(text=f"Unknown error: {e}", status=500)
+        return web.Response(text=f"Database is not available : {e}", status=500)
     if not result:
         return web.Response(text="Key not found", status=404)
     return web.Response(text=result.decode('utf-8'))
 
 
-def main(proxy_ip, proxy_port, client_ip, client_port, cache_capacity, cache_expiry):
+def main(proxy_host, proxy_port, client_host, client_port, cache_capacity, cache_expiry):
     global proxy
 
-    proxy = redis_proxy_factory(client_ip, client_port, cache_capacity, cache_expiry)
+    proxy = redis_proxy_factory(client_host, client_port, cache_capacity, cache_expiry)
     app = web.Application()
     app.router.add_get('/{key}', handle)
 
-    web.run_app(app, host=proxy_ip, port=proxy_port)
+    web.run_app(app, host=proxy_host, port=proxy_port)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Redis proxy server')
-    parser.add_argument('--config', type=str, help='config file', required=True)
-    args = parser.parse_args()
-    cfg = load_config(args.config)
-
-    print(cfg)
-
     main(
-        cfg['proxy']['ip'],
-        cfg['proxy']['port'],
-        cfg['client']['ip'],
-        cfg['client']['port'],
-        cfg['cache']['capacity'],
-        cfg['cache']['expiry']
+        environ['PROXY_HOST'], # cfg['proxy']['ip'],
+        int(environ['PROXY_PORT']), # cfg['proxy']['port'],
+        environ['CLIENT_HOST'], # cfg['client']['ip'],
+        int(environ['CLIENT_PORT']), # cfg['client']['port'],
+        int(environ['CACHE_CAPACITY']), # cfg['cache']['capacity'],
+        int(environ['CACHE_EXPIRY']) # cfg['cache']['expiry']
     )
