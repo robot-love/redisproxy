4c347a480cb384741625620ab2cc6e27b13f375c
Sepehr Ghavam <Sepehr.Ghavam@christiedigital.com>
2022-04-06 11:14:17 -0400

    Setting up a separate container for the RESP protocol proxy. Exposed on the standard redis port 6379, but redirects to proxy, so redis backing instance is not available to outside clients. Impl not tested yet

---
+++ app/server-resp.py
@@ -0,0 +1,37 @@
+from core.proxy import redis_proxy_factory
+
+import logging
+from asyncio import run
+from asyncio.streams import start_server, StreamReader, StreamWriter
+
+
+def parse_resp_get(resp):
+    resp = resp.decode('utf-8')
+    assert resp[:8] == "*2$3GET$"
+    for i,c in enumerate(resp[8:]):
+        if not c.isdigit():
+            return resp[8+i:]
+
+
+async def handle(reader: StreamReader, writer: StreamWriter):
+    data = await reader.read(2048)
+    logging.info(f'New connection: {data}')
+
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+    print(f"Received {message!r} from {addr!r}")
+    print(f"Send: {message!r}")
+
+    writer.write(data)
+    await writer.drain()
+
+
+async def main():
+    srv = await start_server(handle, 'localhost', 8888)
+    addrs = ', '.join(str(sock.getsockname()) for sock in srv.sockets)
+    print(f'Serving on {addrs}')
+
+    async with srv:
+        await srv.serve_forever()
+
+run(main())
---
+++ resp.Dockerfile
@@ -0,0 +1,23 @@
+# syntax=docker/dockerfile:1
+
+FROM python:3.8-slim-buster
+
+WORKDIR /app
+
+ENV PROXY_HOST="0.0.0.0"
+ENV PROXY_PORT=6380
+ENV CLIENT_HOST="redis-db"
+ENV CLIENT_PORT=6379
+ENV CACHE_CAPACITY=10
+ENV CACHE_EXPIRY=10
+ENV CONCURRENT_MAX=2
+
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+
+COPY /app/core /app/core
+COPY /app/test /app/test
+COPY /app/server-resp.py /app/app.py
+
+
+CMD [ "python3", "app.py" ]
